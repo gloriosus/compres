04c1cac63b09a8cb6f335d9d602d130f8cde81fa
Vladislav Bondarenko <vladislav@bondarenko.page>
2022-10-19 14:22:43 +1000

    Add functions to get binary representation of a set, get two-dimensional array and it's optimal size

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import math
-
-def get_sets(_data: str):
-    _sets = []
-    sets_count = math.ceil(len(data) / SET_SIZE)
-    for i in range(0, sets_count):
-        if i == sets_count - 1:
-            _sets.append(_data[i*SET_SIZE..len(data)])
-        _sets.append(_data[i*SET_SIZE..(i+1)*SET_SIZE])
-    return _sets
+import numpy as np
 
 SET_SIZE = 8
 
-data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut arcu tincidunt, pharetra massa sed, " \
-       "dictum nibh. Nunc mattis ipsum vel. "
-sets = get_sets(data)
+
+def get_sets(data: str, size: int):
+    sets = []
+    count = math.ceil(len(data) / size)
+    for i in range(0, count):
+        sets.append(data[i * size:(i + 1) * size])
+    return sets
+
+
+def get_binary(set: str):
+    return "".join(f"{ord(x):08b}" for x in set)
+
+
+def get_matrix(binary: str):
+    matrix = np.array(list(binary), dtype=int)
+    return np.reshape(matrix, (-1, optimal_size(len(matrix))[1]))
+
+
+def optimal_size(length: int):
+    row = 1
+    col = length
+    while row < col:
+        if (row * 2) > (col / 2):
+            break
+        col = col / 2
+        row = row * 2
+    return row, int(col)
+
+
+sample = "helloworldstring"
+result_sets = get_sets(sample, SET_SIZE)
+
+word = get_binary(result_sets[0])
+
+print(word)
+print(get_matrix(word))
